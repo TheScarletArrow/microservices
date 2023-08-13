3c5a186afdfa57cc6cb54eafcc3ceafa32acb651
Anton Yurkov <anton20001701@yandex.ru>
2023-08-13 16:40:54 +0300

    change in signin route

--- auth-service/src/main/java/ru/scarlet/authservice/controller/TokenController.java
+++ auth-service/src/main/java/ru/scarlet/authservice/controller/TokenController.java
@@ -3,20 +3,12 @@ package ru.scarlet.authservice.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.scarlet.authservice.dto.SignInRequest;
-import ru.scarlet.authservice.dto.SignUpRequest;
 import ru.scarlet.authservice.dto.Tokens;
 import ru.scarlet.authservice.dto.UsernameFromToken;
-import ru.scarlet.authservice.entity.User;
-import ru.scarlet.authservice.exceptions.UserAlreadyExistsException;
 import ru.scarlet.authservice.service.TokenService;
 import ru.scarlet.authservice.service.UserService;
 
@@ -47,17 +39,5 @@ public class TokenController {
         } else return ResponseEntity.badRequest().body(null);
     }
 
-    @PostMapping("/signin")
-    public ResponseEntity<User> signin(@RequestBody SignInRequest request, HttpServletRequest httpServletRequest) {
-        User user = userService.getUserByUsername(request.getUsername());
-        return ResponseEntity.ok(user);
-    }
 
-    @PostMapping("/signup")
-    public ResponseEntity<?> signUp(@RequestBody SignUpRequest signUpRequest, HttpServletRequest httpServletRequest) {
-        if (!userService.userExists(signUpRequest.getUsername())) {
-            User user = userService.createUser(signUpRequest);
-            return new ResponseEntity<>(user, HttpStatusCode.valueOf(201));
-        } else throw new UserAlreadyExistsException();
-    }
 }
