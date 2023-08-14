9832c6d1c24d4026708b2db3ef22e32146bd168a
Anton Yurkov <anton20001701@yandex.ru>
2023-08-14 19:46:25 +0300

    get user by username

--- auth-service/src/main/java/ru/scarlet/authservice/controller/UserController.java
+++ auth-service/src/main/java/ru/scarlet/authservice/controller/UserController.java
@@ -4,10 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.scarlet.authservice.dto.SignInRequest;
 import ru.scarlet.authservice.dto.SignUpRequest;
 import ru.scarlet.authservice.entity.User;
@@ -32,5 +29,11 @@ public class UserController {
             User user = userService.createUser(signUpRequest);
             return new ResponseEntity<>(user, HttpStatusCode.valueOf(201));
         } else throw new UserAlreadyExistsException();
+    }
+
+    @GetMapping
+    public ResponseEntity<User> getUserByUsername(@RequestParam String username) {
+        User userByUsername = userService.getUserByUsername(username);
+        return ResponseEntity.ok(userByUsername);
     }
 }
