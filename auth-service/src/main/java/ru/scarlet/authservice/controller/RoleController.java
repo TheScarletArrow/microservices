b336992ded5b8a8b9d6c62d9af49f11795852105
Anton Yurkov <anton20001701@yandex.ru>
2023-08-20 22:12:08 +0300

    logger implementation

--- auth-service/src/main/java/ru/scarlet/authservice/controller/RoleController.java
+++ auth-service/src/main/java/ru/scarlet/authservice/controller/RoleController.java
@@ -3,6 +3,8 @@ package ru.scarlet.authservice.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.MDC;
 import org.springframework.web.bind.annotation.*;
 import ru.scarlet.authservice.dto.RoleRequest;
 import ru.scarlet.authservice.dto.RoleResponse;
@@ -14,19 +16,22 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/roles")
 @RequiredArgsConstructor
-
+@Slf4j
 public class RoleController {
     private final RoleService roleService;
+
 
     @GetMapping("/")
 @   Operation(description = "Get all roles", summary = "Get all roles")
     public List<RoleResponse> getRoles(){
+        log.info("{} getRoles()", MDC.get("Corr"));
         return roleService.getAll();
     }
 
     @PostMapping("/")
     @Operation(description = "Create new role", summary = "Create new role")
     public Role addRole(@RequestBody RoleRequest roleRequest){
+        log.info("{} addRole({})", MDC.get("Corr"), roleRequest);
         return roleService.addRole(roleRequest);
     }
 }
---
+++ auth-service/src/main/java/ru/scarlet/authservice/filters/AuthFilter.kt
@@ -0,0 +1,35 @@
+package ru.scarlet.authservice.filters
+
+import org.slf4j.LoggerFactory
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import org.slf4j.MDC
+import org.springframework.stereotype.Component
+
+const val STRING_LENGTH = 11
+
+@Component
+class AuthFilter : Filter {
+    companion object {
+        @Suppress("JAVA_CLASS_ON_COMPANION")
+        @JvmStatic
+        private val logger = LoggerFactory.getLogger(javaClass.enclosingClass)
+    }
+
+    private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+
+    override fun doFilter(httpServletRequest: ServletRequest, httpServletResponse: ServletResponse, filterChain: FilterChain) {
+
+        val randomString = (1..STRING_LENGTH)
+            .map { i -> kotlin.random.Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+
+        MDC.put("Corr", randomString)
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse)
+
+    }
+}
