56416d53731be0af234af6dbe2c783d0e4b3c635
Anton Yurkov <anton20001701@yandex.ru>
2023-08-20 21:50:51 +0300

    openapi

--- auth-service/src/main/java/ru/scarlet/authservice/controller/RoleController.java
+++ auth-service/src/main/java/ru/scarlet/authservice/controller/RoleController.java
@@ -1,5 +1,7 @@
 package ru.scarlet.authservice.controller;
 
+
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import ru.scarlet.authservice.dto.RoleRequest;
@@ -12,15 +14,18 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/roles")
 @RequiredArgsConstructor
+
 public class RoleController {
     private final RoleService roleService;
 
     @GetMapping("/")
+@   Operation(description = "Get all roles", summary = "Get all roles")
     public List<RoleResponse> getRoles(){
         return roleService.getAll();
     }
 
     @PostMapping("/")
+    @Operation(description = "Create new role", summary = "Create new role")
     public Role addRole(@RequestBody RoleRequest roleRequest){
         return roleService.addRole(roleRequest);
     }
