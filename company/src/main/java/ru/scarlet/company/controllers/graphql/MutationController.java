868b21e010d9f9f241a649fd22b9957928db2073
Anton Yurkov <anton20001701@yandex.ru>
2023-10-15 13:56:06 +0300

    transfer some rest queries to graphql

--- company/src/main/java/ru/scarlet/company/controllers/graphql/MutationController.java
+++ company/src/main/java/ru/scarlet/company/controllers/graphql/MutationController.java
@@ -1,10 +1,12 @@
-package ru.scarlet.company.controllers;
+package ru.scarlet.company.controllers.graphql;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Controller;
+import ru.scarlet.company.entities.Course;
 import ru.scarlet.company.entities.Professor;
+import ru.scarlet.company.services.CourseService;
 import ru.scarlet.company.services.ProfessorService;
 
 @Controller
@@ -12,9 +14,17 @@ import ru.scarlet.company.services.ProfessorService;
 public class MutationController {
 
     private final ProfessorService professorService;
+    private final CourseService courseService;
 
     @MutationMapping
     public Professor updateProfessorContactInfo(@Argument Integer id, @Argument String email, @Argument String phone){
        return  professorService.setProperties(id, email, phone);
+    }
+
+    @MutationMapping
+    public Course addProfessorToCourse(@Argument Integer professorId, @Argument Integer courseId){
+        courseService.addProfessor(courseId, professorId);
+
+        return courseService.getCourseByIdE(courseId);
     }
 }
--- company/src/main/java/ru/scarlet/company/controllers/graphql/QueryController.java
+++ company/src/main/java/ru/scarlet/company/controllers/graphql/QueryController.java
@@ -1,4 +1,4 @@
-package ru.scarlet.company.controllers;
+package ru.scarlet.company.controllers.graphql;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
