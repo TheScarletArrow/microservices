7049846bbeec3b402444d138367858b0bb589dc8
Anton Yurkov <anton20001701@yandex.ru>
2023-12-09 16:56:36 +0300

    BuildInfo

---
+++ company/src/main/java/ru/scarlet/company/controllers/BasicController.kt
@@ -0,0 +1,28 @@
+package ru.scarlet.company.controllers
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
+import org.springframework.boot.info.GitProperties
+import org.springframework.boot.info.InfoProperties
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.scarlet.company.dtos.Info
+
+@RestController
+@RequestMapping("/api/v1/")
+class BasicController {
+    @Autowired
+    private val buildProperties: BuildProperties? = null
+
+    @Autowired
+    private val gitProperties: GitProperties? = null
+
+
+    @GetMapping("/")
+    fun root(): ResponseEntity<Info> {
+        return ResponseEntity(Info(buildProperties, gitProperties), HttpStatus.OK)
+    }
+}
