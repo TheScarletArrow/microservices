75ac86c4d35e30b50bc8b24573768fcb9f4e6f1b
Anton Yurkov <anton20001701@yandex.ru>
2023-12-24 22:46:33 +0300

    mail

--- notifications/src/main/kotlin/ru/scarlet/notifications/service/impl/EmailSenderServiceImpl.kt
+++ notifications/src/main/kotlin/ru/scarlet/notifications/service/impl/EmailSenderServiceImpl.kt
@@ -2,6 +2,8 @@ package ru.scarlet.notifications.service.impl
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
 import ru.scarlet.notifications.service.EmailSenderService
@@ -15,7 +17,18 @@ class EmailSenderServiceImpl(
         val logger: Logger = LoggerFactory.getLogger(this::class.java)
     }
 
+    @Value("\${spring.mail.username}")
+    private val from: String? = null
+
     override fun sendEmail(destination: String) {
         logger.info("sendEmail {}", destination)
+
+        val message = SimpleMailMessage()
+        message.text = "Приветствуем на портале!"
+        message.setTo(destination)
+        message.from = from
+        message.subject = "Приветствуем на портале"
+        javaMailSender?.send(message)
+        logger.info("Sent message to {}", destination)
     }
 }
